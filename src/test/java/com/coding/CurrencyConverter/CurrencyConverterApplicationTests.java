8bff8d92dbbd3006f4ee2dd4f5996f5892e747bb
ulan <ulansatybaldiev6@gmail.com>
2023-03-09 01:14:29 +0100

    initial setup

---
+++ src/test/java/com/coding/CurrencyConverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.CurrencyConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
